3fd59b7b6e191812a3d837cc0cd4c4b412eb5a8d
shiro105 <shiro105@mail.chapman.edu>
2017-10-01 00:03:40 -0700

    Initial Commit

---
+++ CardStackApp/app/src/main/java/cpsc356/cardstackapp/MainActivity.java
@@ -0,0 +1,13 @@
+package cpsc356.cardstackapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
